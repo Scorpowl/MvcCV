de0e46a55fe5fccadd923dea6d3afef97f1670d4
Furkan TOSUN <furkann.1777@outlook.com>
2025-03-15 13:59:58 +0300

    Hakkımda düzenlemesi

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcCV.Models.Entity;
 
 namespace MvcCV.Controllers
+    
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        DbCvEntities db = new DbCvEntities();
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
         }
     }
 }
