d36c7c0530fc31b79ec1051810103fdc8edcad06
Furkan TOSUN <furkann.1777@outlook.com>
2025-03-26 16:41:53 +0300

    güncellemev0.1

---
+++ MvcCV/MvcCV/Controllers/DefaultController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCV.Models.Entity;
+
+namespace MvcCV.Controllers
+    
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblDeneyimlerim.ToList();
+            return PartialView(deneyimler);
+        }public PartialViewResult Eğitimlerim()
+        {
+            var egitimler = db.TblEgitimlerim.ToList();
+            return PartialView(egitimler);
+        }public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblYeteneklerim.ToList();
+            return PartialView(yetenekler);
+        }public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblHobilerim.ToList();
+            return PartialView(hobiler);
+        }public PartialViewResult Sertifikalar()
+        {
+            var sertifika = db.TblSertifikalarım.ToList();
+            return PartialView(sertifika);
+        }
+        [HttpGet]
+        public PartialViewResult Iletisim()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public PartialViewResult Iletisim(TblIletisim t)
+        {
+            db.TblIletisim.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+    }
+}
